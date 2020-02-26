579c19e58a321081a497516410c33ddd69f5572b
Gabor Czene <gabor@czene.co.uk>
2020-02-26 07:52:33 +0000

    preloader

--- app/components/top-menu/component.js
+++ app/components/top-menu/component.js
@@ -1,4 +1,11 @@
 import Component from '@glimmer/component';
 
 export default class TopMenuComponent extends Component {
+  constructor() {
+    super(...arguments);
+    const el = document.getElementById('pre-loader');
+    if (el) {
+      el.parentNode.removeChild(el);
+    }
+  }
 }
